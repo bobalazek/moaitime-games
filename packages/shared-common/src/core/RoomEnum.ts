00e1cfdf4d4a97008d2d4af8418de6d0a51eee28
Borut Balazek <bobalazek124@gmail.com>
2024-01-13 19:41:35 +0100

    Cleanup

--- packages/shared-common/src/core/RoomEnum.ts
+++
@@ -1,4 +0,0 @@
-export enum RoomEnum {
-  LOBBY = 'lobby',
-  GAME = 'game',
-}
--- packages/shared-common/src/index.ts
+++ packages/shared-common/src/index.ts
@@ -2,7 +2,6 @@
 export * from './core/Constants';
 export * from './core/DeviceTypeEnum';
 export * from './core/DevicePlatformEnum';
-export * from './core/RoomEnum';
 
 // Session
 export * from './session-state/Session';
